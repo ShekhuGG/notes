0fed228418702a760876f13ff61b0a9e77e55f49
ShekhuGG <shekharshawcoc@gmail.com>
2025-01-30 12:33:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,571 @@
+let arr = [`I found a book called How to Solve 50% of Your Problems. So I bought 2.`, `
+    What do Keanu Reeves and baby Yoda have in common? They age at the same rate.`, `
+    Why did the coffee taste like dirt? Because it was ground just a few minutes ago.`, `
+    Why can't you make a dinosaur omelet? Because they're egg-stinct.`, `
+    Why do quarterbacks tell obvious jokes? So they don't go over their receivers' heads.`, `
+    Why did the Rolling Stones stop making music? Because they got to bottom of the hill.`, `
+    How many goats does it take to change a lightbulb? Just one, but you have to goat them into it.`, `
+    What did one cupcake say to the other? You ain't see muffin yet.`, `
+    What is the best present? Broken drums! You can't beat them.`, `
+    Did you hear about the new squirrel diet? It's just nuts.`, `
+    I made song about tortilla once, now it's more like a wrap.`, `
+    Did you hear about the spatula's hot new flame? It met the grill of its dreams.`, `
+    Did you know corduroy pillows are in style? They're making headlines.`
+
+, ` What did the policeman say to his hungry stomach? “Freeze. You’re under a vest.”`
+
+, ` What did the left eye say to the right eye? Between us, something smells!`
+
+, ` What do you call a guy who’s really loud? Mike.`
+
+, ` Why do birds fly south in the winter? It’s faster than walking!`
+
+, ` What did the lava say to his girlfriend? “I lava you!”`
+
+, ` Why did the student eat his homework? Because the teacher told him it was a piece of cake.`
+
+, ` What did Yoda say when he saw himself in 4k? HDMI.`
+
+, ` Which superhero hits home runs? Batman!`
+
+, ` What’s Thanos’ favorite app on his phone,chat.`
+
+, `Sandy’s mum has four kids; North, West, East. What is the name of the fourth child? Sandy, obviously!`
+
+, ` What is a room with no walls? A mushroom.`
+
+, ` Why did the blue jay get in trouble at school? For tweeting on a test!`
+
+, ` What social events do spiders love to attend? Webbings.`
+
+, ` What did one pickle say to the other? Dill with it.`, ` What is brown, hairy and wears sunglasses? A coconut on vacation.`
+
+, ` Why is a football stadium always cold? It has lots of fans!`
+
+, `  What did one math book say to the other? “I’ve got so many problems.”`
+
+, ` What did the Dalmatian say after lunch? That hit the spot!`
+
+, ` What do you call two bananas on the floor? Slippers.`
+
+, ` Why did the chicken cross the playground? To get to the other slide.`
+
+, ` Why do ducks have feathers ,eir tails? To cover their butt quacks.`
+
+, ` How does a vampire start a letter? “Tomb it may concern…”`
+
+, ` A plane crashed in the jungle and every single person died. Who survived? Married couples.`
+
+, ` What kind of math do birds love? Owl-gebra!`
+
+, ` Why can’t you ever tell a joke around glass? It could crack up.`, `What falls, but never needs a bandage? The rain`,
+`I was going to tell you a joke about boxing but I forgot the punch line`,
+`I'm not a fan of spring cleaning. Let's be honest, I'm not into summer, fall, or winter cleaning either`,
+`Why did the egg hide? It was a little chicken`,
+`What did the dirt say to the rain? If you keep this up, my name will be mud!
+    Why couldn't the sunflower ride its bike? It lost its petals`,
+`What's an egg's favorite vacation spot? New Yolk City`,
+`I ate a sock yesterday. It was very time-consuming`,
+`What kind of candy do astronauts like? Mars bars`,
+`I wanted to buy some camo pants but couldn't find any`,
+`I ordered a chicken and an egg from Amazon. I'll let you know`,
+`What month is the shortest of the year? May, it only has three letters`,
+`What did the snail who was riding on the turtle's back say? Wheeeee!
+    I was going to tell a time traveling joke, but you guys didn't like it`,
+`What do you call a lazy kangaroo? A pouch potato`,
+`I used to run a dating service for chickens, but I was struggling to make hens meet`,
+`Why do we tell actors to "break a leg?" Because every play has a cast`,
+`What does a pig put on dry skin? Oinkment`,
+`What do you call it when a snowman throws a tantrum? A meltdown`,
+`My uncle named his dogs Timex and Rolex. They're his watch dogs`,
+`Did you hear about the guy whose left side was cut off? He's all right now`,
+`How do you open a banana? With a mon-key`,
+`Which is faster, hot or cold? Hot, because you can catch cold`,
+`What did one plate say to the other plate? Dinner's on me`,
+`Why do oranges wear sunscreen? So they don't peel`,
+`My wife told me to stop acting like a flamingo, so I had to put my foot down`,
+`What do you call a pig that does karate? A pork chop`,
+`Where does Batman go to the bathroom? The batroom`,
+`What do you call a pony with a sore throat? A little horse`,
+`What did the left eye say to the right eye? Between you and me, something smells`,
+`What did the mama tomato say to the baby tomato? Catch up!
+    Why didn't the melons get married? Because they cantaloupe`,
+`What do you call a fake noodle? An impasta`,
+`How did the pig get to the hogspital? In a hambulance`,
+`I'm so good at sleeping I can do it with my eyes closed!
+    Why does Humpty Dumpty love autumn? Because he had a great fall`,
+`What happens when a strawberry gets run over crossing the street? Traffic jam`,
+`Why did the cow jump over the moon? The farmer had cold hands`,
+`A termite walks into a bar and says, "So, is the bar tender here?"
+    How does an octopus go into battle? Well-armed`,
+`What do you call a pudgy psychic? A four-chin teller`,
+`What do you get when you mix a cocker spaniel, a poodle, and a ghost? A cocker-poodle boo`,
+`How do celebrities stay cool? They have many fans`,
+`What does a pickle say when he wants to play cards? "Dill me in!"
+    How much money does a pirate pay for corn? A buccaneer`,
+`Where do young trees go to learn? Elementree school`,
+`Why do bees have sticky hair? Because they use a honeycomb`,
+`How did the student feel when he learned about electricity? Totally shocked`,
+`What do you call a bee that can't make up its mind? A Maybe`,
+`Why was six afraid of seven? Because 7-8-9`,
+`If April showers bring May flowers, what do May flowers bring? Pilgrims`,
+`I tried to catch fog yesterday. Mist`,
+`What do you call a hippie's wife? Mississippi`,
+`Two peanuts were walking down the street. One was a-salted`,
+`How can you tell it’s a dogwood tree? By the bark`,
+`What did the buffalo say when his kid went to college? Bison`,
+`What did the mayonnaise say when the refrigerator door was opened? Close the door, I'm dressing`,
+`What's the stinkiest planet? Poopiter`,
+`What did one wall say to the other? I'll meet you at the corner`,
+`Why don't sharks eat clowns? Because they taste funny`,
+`A horse walks into a bar. The bartender says, "Why the long face?"
+    What did the pirate say when he turned 80? Aye matey`,
+`What's black and white and goes round and round? A penguin in the washing machine`,
+`How do you organize a space party? You planet`,
+`Why couldn't the bicycle stand up by itself? It was two tired`,
+`Did you hear the rumor about butter? Well, I'm not going to spread it`,
+`Why did the student eat his homework? Because his teacher told him it was a piece of cake`,
+`What did one hat say to the other? You wait here, I'll go on ahead`,
+`What do you call a dinosaur that crashes his car? Tyrannosaurus Wrecks`,
+`I couldn't figure out why the baseball kept getting larger. Then it hit me`,
+`What do you call a boomerang that won’t come back? A stick`,
+`What did the full glass say to the empty glass? You look drunk`,
+`How do you stop a bull from charging? Cancel its credit card`,
+`Why don’t we see elephants hiding in trees? Because they’re really good at it`,
+`What’s the difference between a hippo and a zippo? One’s pretty heavy and the other’s a little lighter`,
+`Did you hear the one about the roof? Never mind, it's over your head`,
+`I used to hate facial hair...but then it grew on me`,
+`A cheese factory exploded in France. Da brie was everywhere`,
+`What's a ninja's favorite type of shoes? Sneakers`,
+`What's the best smelling insect? A deodor-ant`,
+`What do you call a bear without any teeth? A gummy bear`,
+`Why was the coach yelling at the vending machine? He wanted his quarter back.`]
+let arr2 = []
+let arr3 = []
+let arr4 = []
+let cno;
+const color = () => {
+cno = Math.floor(Math.random() * 8)
+console.log(cno, arr2[0])
+while (cno == arr2[0]) {
+cno = Math.floor(Math.random() * 8)
+}
+if (cno == 0) {
+return 'orange'
+}
+if (cno == 1) {
+return 'lightblue'
+}
+if (cno == 2) {
+return 'yellow'
+}
+if (cno == 3) {
+return 'lightgreen'
+}
+if (cno == 4) {
+return 'pink'
+}
+if (cno == 5) {
+return 'white'
+}
+if (cno == 6) {
+return 'cyan'
+}
+if (cno == 7) {
+return 'red'
+}
+}
+
+//Reload joke 
+
+let random = Math.floor(Math.random() * arr.length)
+arr3.push(random)
+
+document.getElementById('joke').innerHTML = arr[random]
+document.getElementById('intro').innerHTML = `JOKE NO : ${random + 1}`
+let clr = color()
+document.getElementById('joke').style.color = clr
+arr2.pop()
+arr2.push(cno)
+console.log(arr2[0])
+
+// shuffle effect
+let btn = document.getElementById('button')
+function click_btn() {
+btn.click()
+}
+let strt = 0
+let times = 1
+while (strt < 60) {
+times = strt * 50
+setTimeout(click_btn, times)
+strt++
+}
+
+//Shuffled Joke
+let jn;
+let rndm_likes
+document.getElementById('button').addEventListener('click', function() {
+jn = Math.floor(Math.random() * arr.length)
+while (jn == arr3[0]) {
+jn = Math.floor(Math.random() * arr.length)
+}
+document.getElementById('joke').innerHTML = arr[jn];
+document.getElementById('joke').style.color = color()
+document.getElementById('intro').innerHTML = `JOKE NO : ${jn*123 + 1}`
+rndm_likes = rndm_like()
+arr4.unshift(rndm_likes)
+console.log(cno, arr2[0]);
+arr3.pop();
+arr3.push(jn);
+arr2.pop();
+arr2.push(cno);
+console.log(cno, arr2[0]);
+console.log(i1)
+if (i1 % 2 == 1) {
+document.getElementById(`thumb`).click()
+}
+});
+
+// Theme Changer
+
+const theme = () => {
+cno = Math.floor(Math.random() * 8)
+while (cno == arr2[0]) {
+cno = Math.floor(Math.random() * 8)
+}
+if (cno == 0) {
+return 'orange'
+}
+if (cno == 1) {
+return 'lightblue'
+}
+if (cno == 2) {
+return 'yellow'
+}
+if (cno == 3) {
+return 'lightgreen'
+}
+if (cno == 4) {
+return 'pink'
+}
+if (cno == 5) {
+return 'white'
+}
+if (cno == 6) {
+return 'cyan'
+}
+if (cno == 7) {
+return 'royalblue'
+}
+}
+
+const img = (x) => {
+switch (x) {
+case (1): {
+return `https://img.freepik.com/free-vector/floating-laughing-emoji-characters_52683-27761.jpg`
+}
+case (2): {
+return `https://wallpapers.com/images/featured/laughing-background-vxaw6wsm4i73ybsi.jpg`
+}
+case (3): {
+return `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlNNe2JYiZP13lw829h5Cpp9B1QuWccu8XqQ&s`
+}
+case (4): {
+return `https://th.bing.com/th/id/R.e915d62fcba8f024c65993dd7ef5e9d8?rik=3Dop4mg6K8uFMw&riu=http%3A%2F%2Fi.huffpost.com%2Fgen%2F4298368%2Fimages%2Fo-BABY-LAUGHING-facebook.jpg&ehk=0ZpJnqwemFp3v4FAL4tooy628o3HPTvdknXyMawk95U%3D&risl=&pid=ImgRaw&r=0`
+}
+case (5): {
+return `https://t3.ftcdn.net/jpg/06/53/25/88/360_F_653258868_AXJOw764UqhaeFeebCjtamCFocAWb7Yk.jpg`
+}
+case (6): {
+return `https://e1.pxfuel.com/desktop-wallpaper/195/426/desktop-wallpaper-funny-emoji-new-tab-theme-laughing-or-crying-laughing-emoji.jpg`
+}
+case (7): {
+return `https://wallpapers.com/images/hd/laughing-pictures-6bo91efcosjnvtvz.jpg`
+}
+case (8): {
+return `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5L_yUOQ9vq7ehlM5OQSgAacto3Mf6wOjF8A&s`
+}
+case (9): {
+return `https://www.shutterstock.com/image-vector/group-people-laughing-lol-isolated-260nw-1390409888.jpg`
+}
+case (10): {
+return `https://previews.123rf.com/images/artfirst/artfirst2312/artfirst231222743/219153289-winter-holiday-christmas-background-banner-closeup-of-cute-funny-laughing-snowman-with-wool-hat-and.jpg`
+}
+case (11): {
+return `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbRqCLy-kV5rlmNRsuWnp2aswNQn2knnjmuw&s`
+}
+case (12): {
+return `https://png.pngtree.com/background/20230517/original/pngtree-two-muddy-youngsters-on-the-ground-laughing-picture-image_2637171.jpg`
+}
+case (13): { 
+return `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6l87lCY-RHzppCk8LrCLM5Dvs6JIfKAAyKTftwu7l4Q&s`
+}
+}
+}
+let tcolor;
+document.getElementById('button1').addEventListener('click', function() {
+tcolor = theme()
+console.log(tcolor)
+document.getElementById('jokebox').style.boxShadow = tcolor.concat(` 0px 0px 90px 5px`)
+document.getElementById('joke').style.textShadow = `1px 1px 10px  ${tcolor}`
+console.log(arr2[0], cno)
+arr2.pop()
+arr2.push(cno)
+console.log(arr2)
+})
+
+// Brightness of background 
+
+let btns = document.getElementById('btns')
+let bgndlev = document.getElementById('bgndlev')
+let bgnd = document.getElementById('bgnd')
+let cut = document.getElementById('cut')
+let image = document.getElementById('button2')
+let lastlev = `35`
+let btnsi = 0;
+
+let opc;
+document.getElementById('submit').addEventListener('click', function() {
+opc = bgndlev.value
+document.getElementsByClassName('bi')[0].style.opacity = `${opc}%`
+})
+btns.addEventListener('click', function() {
+bgnd.style.display = 'flex'
+btns.style.display = 'none'
+cut.style.display = 'block'
+bgndlev.focus()
+btnsi++
+
+console.log(lastlev)
+
+if (lastlev <= 100 && lastlev != ``) {
+bgndlev.placeholder = `${lastlev}`
+}
+
+else if (lastlev == ``) {
+bgndlev.placeholder = `35`
+}
+else {
+bgndlev.placeholder = `100`
+};
+
+})
+
+document.getElementById('submit').addEventListener('click', function() {
+lastlev = bgndlev.value
+if (lastlev == `69`) {
+document.getElementById('joke').style.color = 'red'
+}
+})
+
+cut.addEventListener('click', function() {
+bgnd.style.display = 'none'
+cut.style.display = 'none'
+btns.style.display = 'inline-block'
+bgndlev.value = ''
+btnsi++
+})
+
+
+bgndlev.addEventListener('input', function() {
+if (bgndlev.value < 0 || bgndlev.value == '-') {
+bgndlev.value = ''
+}
+})
+let i = 0
+let imgvalue;
+image.addEventListener('click', function() {
+if (i > 13) {
+i = 0
+}
+// document.getElementById('bi').style.backgroundImage = `url(${imgvalue})`
+for (k = 0; k < 14; k++) {
+document.getElementById('bi' + k).classList.remove('bi')
+}
+document.getElementById('bi' + i).classList.toggle('bi')
+imgvalue = img(++i)
+if (i < 14) {
+document.getElementsByTagName('footer')[0].style.backgroundImage = `url(${imgvalue})`
+}
+document.getElementsByTagName('footer')[0].style.backgroundSize = `cover`
+console.log(i)
+console.log(img(i))
+document.getElementsByClassName('bi')[0].style.backgroundSize = 'cover'
+document.getElementsByClassName('bi')[0].style.opacity = `${Number(ibi) - 8}%`
+document.getElementsByClassName('bi')[0].style.opacity = `${Number(ibi) + 5}%`
+document.getElementsByClassName('bi')[0].style.opacity = `${opc}%`
+})
+
+// additional clicks
+
+document.addEventListener('keydown', function(event) {
+if (btnsi % 2 != 1 && event.key === 'Enter' || event.key === 'ArrowRight') {
+document.getElementById('button').click();
+}
+});
+
+document.addEventListener('keydown', function(event) {
+if (event.key === 'B' || event.key === 'b') {
+if (event.ctrlKey) {
+document.getElementById('btns').click()
+}
+else {
+document.getElementById('button2').click()
+}
+}
+});
+document.addEventListener('keydown', function(event) {
+if (event.key === 'T' || event.key === 't') {
+document.getElementById('button1').click();
+}
+});
+
+// refrence display when mouseenters
+
+btns.addEventListener('mouseover', function() {
+let isover = true;
+btns.addEventListener('mouseout', function() {
+isover = `false`
+});
+console.log(isover, `isover2`);
+setTimeout(function() {
+if (isover != `false`) {
+document.getElementById('key-ctrl-b').style.display = 'inline'
+}
+}, 450)
+})
+
+btns.addEventListener('mouseout', function() {
+document.getElementById('key-ctrl-b').style.display = 'none'
+})
+
+
+
+
+button.addEventListener('mouseover', function() {
+let isover = true;
+button.addEventListener('mouseout', function() {
+isover = `false`
+});
+setTimeout(function() {
+if (isover != `false`) {
+document.getElementById('key-enter').style.display = 'inline'
+}
+}, 450)
+button.addEventListener(`click`, function() {
+document.getElementById(`likes`).click()
+})
+})
+button.addEventListener('mouseout', function() {
+document.getElementById('key-enter').style.display = 'none'
+})
+
+
+
+button1.addEventListener('mouseover', function() {
+let isover = true;
+button1.addEventListener('mouseout', function() {
+isover = `false`
+});
+console.log(isover, `isover2`);
+setTimeout(function() {
+if (isover != `false`) {
+document.getElementById('key-t').style.display = 'inline'
+}
+}, 450)
+})
+button1.addEventListener('mouseout', function() {
+document.getElementById('key-t').style.display = 'none'
+})
+
+
+
+button2.addEventListener('mouseover', function() {
+let isover = true;
+button2.addEventListener('mouseout', function() {
+isover = `false`
+});
+setTimeout(function() {
+if (isover != `false`) {
+document.getElementById('key-b').style.display = 'inline'
+}
+}, 450)
+})
+button2.addEventListener('mouseout', function() {
+document.getElementById('key-b').style.display = 'none'
+})
+
+
+// random like
+
+let rndm_like = () => {
+let i = Math.floor((Math.random()) * 112 + 50)
+return `${i}.` + `${Math.floor((Math.random()) * 10)}k`
+}
+
+let i1 = 0
+document.getElementById('thumb').addEventListener('click', function() {
+i1++
+console.log(i1)
+if (i1 % 2 == 1) {
+document.getElementById('click').innerHTML = `${rndm_likes}`
+document.getElementById('likes').style.flexDirection = `row`
+// document.getElementById('thumb').style.right = `80px`
+document.getElementsByClassName('fa-thumbs-up')[0].style.color = 'blue'
+}
+else {
+document.getElementById('thumb').style.position = `static`
+document.getElementById('click').innerHTML = ``
+document.getElementById('likes').style.flexDirection = `row-reverse`
+document.getElementsByClassName('fa-thumbs-up')[0].style.color = 'rgba(255,255,255,0.5)'
+document.getElementById('likes').style.right = `0`
+}
+})
+document.getElementsByClassName(`fa-thumbs-up`)[0].addEventListener('mouseenter', function() {
+console.log(`Enter`)
+if (i1 % 2 == 1) {
+document.getElementsByClassName(`fa-thumbs-up`)[0].style.color = `rgba(255,0,100,0.4)`
+}
+else {
+this.style.color = `rgba(255,255,255,0.75)`
+}
+})
+document.getElementsByClassName(`fa-thumbs-up`)[0].addEventListener('mouseout', function() {
+console.log(`Out`)
+if (i1 % 2 == 1) {
+document.getElementsByClassName(`fa-thumbs-up`)[0].style.color = `#0062ff`
+}
+else {
+this.style.color = `rgba(255,255,255,0.5)`
+}
+})
+document.addEventListener('keydown', function() {
+if (btnsi % 2 == 1 && event.key === 'Enter') {
+document.getElementById(`submit`).click()
+}
+})
+document.addEventListener('keydown', function() {
+if (event.key === 'Delete') {
+cut.click()
+}
+})
+let ibi = Number(lastlev)
+document.addEventListener('keydown', function(event) {
+if (event.ctrlKey && event.key === 'ArrowUp') {
+console.log(ibi, `first ibi`, ibi + 8)
+document.getElementsByClassName('bi')[0].style.opacity = `${Number(ibi) + 8}%`
+console.log(ibi, `midway`)
+ibi += 8
+console.log(ibi, `second ibi`)
+lastlev = `${ibi}`
+}
+else if (event.ctrlKey && event.key === 'ArrowDown') {
+console.log(ibi, `first ibi`, ibi - 5)
+document.getElementsByClassName('bi')[0].style.opacity = `${Number(ibi) - 5}%`
+console.log(ibi, `midway`)
+ibi -= 5
+console.log(ibi, `second ibi`)
+lastlev = `${ibi}`
+}
+})
